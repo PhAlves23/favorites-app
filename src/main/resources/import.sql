bd1aea57157bddd9424e418b4e2a22e03531dc41
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2023-03-07 18:19:13 -0300

    feat: create seeding DB

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO TB_USUARIO(nome, email, senha, telefone) VALUES ('Paulo Henrique', 'ph23.alves@gmail.com', '123', '11972266510');
+INSERT INTO TB_USUARIO(nome, email, senha, telefone) VALUES ('Vanessa Camargo', 'vanessa.camargo@gmail.com', '123456', '11823344562');
+INSERT INTO TB_USUARIO(nome, email, senha, telefone) VALUES ('Alexandre Neto', 'ale.neto@gmail.com', '123', '11946677654');
